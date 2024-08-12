2a340c3789c37959d606ec15d6f672058b34f0e0
songexile <psg3398@autuni.ac.nz>
2024-08-12 18:33:55 +1200

    Updated service card and tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,27 @@ export default {
   daisyui: {
     themes: [
       {
-        cmyk: {
-          ...require("daisyui/src/theming/themes")["cmyk"],
-          primary: "#f97316",
-          primaryfocus: "#f97316",
-          secondary: "#2d2e2d",
+        mytheme: {
+          "primary": "#2563EB",
+          "primary-focus": "#1d4ed8",
+          "primary-content": "#ffffff",
+          "secondary": "#f3f4f6",
+          "secondary-focus": "#e5e7eb",
+          "secondary-content": "#1f2937",
+          "accent": "#37cdbe",
+          "accent-focus": "#2aa79b",
+          "accent-content": "#ffffff",
+          "neutral": "#3d4451",
+          "neutral-focus": "#2a2e37",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#f3f4f6",
+          "base-300": "#d1d5db",
+          "base-content": "#1f2937",
+          "info": "#2094f3",
+          "success": "#009485",
+          "warning": "#ff9900",
+          "error": "#ff5724",
         },
       },
     ],
