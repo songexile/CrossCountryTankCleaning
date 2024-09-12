8a021df4db74abf93d80fafcf887e455b0acbcc8
songexile <psg3398@autuni.ac.nz>
2024-09-12 15:53:25 +1200

    ADDED site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()],
+  site: 'https://ccwtc.co.nz/',
+  integrations: [tailwind(), react(), icon(), sitemap()],
   redirects: {
-    "/old-page": "/new-page",
-  },
+    "/old-page": "/new-page"
+  }
 });
